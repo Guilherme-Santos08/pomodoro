589f53e8cfb98ec14056205d60d9adac07d0ebf9
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-07-27 15:13:53 -0300

    Layout

--- src/components/Pomodoro/Pomoro.jsx
+++ src/components/Pomodoro/Pomoro.jsx
@@ -1,17 +1,24 @@
-import { Teste } from "./styles"
+import { Teste } from "./styles";
 
 const Pomodoro = () => {
    return (
       <Teste>
-         <div>
+         <div className="teste">
             <header>
                <li>Pomodoro</li>
                <li>Short Break</li>
                <li>Long Break</li>
             </header>
+            <div className="counter">
+               <div className="counter__info">
+                  <span>24:59</span>
+                  <button>Start</button>
+                  {/* Criar um ternario, quando o counter estiver ativo, a menssagem muda pra "Stop"*/}
+               </div>
+            </div>
          </div>
       </Teste>
    );
-}
- 
+};
+
 export default Pomodoro;
--- src/components/Pomodoro/styles.js
+++ src/components/Pomodoro/styles.js
@@ -11,12 +11,14 @@ export const Teste = styled.div`
    height: 100vh;
    padding: 0 10px;
 
-   div {
+   .teste {
       background-color: #00303d;
       width: 677px;
       height: 472px;
       border-radius: 8px;
       padding: 10px;
+      display: flex;
+      flex-direction: column;
 
       header {
          background-color: #005066;
@@ -37,10 +39,78 @@ export const Teste = styled.div`
          }
       }
 
-      @media (max-width: 425px) {
+      .counter {
+         width: 423px;
+         height: 240px;
+
+         border-radius: 8px;
+         
+         margin: auto;
+
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         justify-content: center;
+
+         background-color: #005066;
+
+         .counter__info {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+         }
+
+         span {
+            font-size: 90px;
+            font-weight: bold;
+         }
+
+         button {
+            font-size: 1.5rem;
+            font-weight: bold;
+            text-transform: uppercase;
+            color: white;
+
+            width: 285px;
+            height: 67px;
+
+            margin-top: 20px;
+            background-color: #00607a;
+
+            transform: scale(0.9);
+
+            border: 2px solid black;
+            border-radius: 8px;
+
+            cursor: pointer;
+         }
+      }
+
+      @media (max-width: 450px) {
          header {
             li {
                font-size: 1rem;
+            }
+         }
+         .counter {
+            width: 100%;
+            height: 240px;
+
+            span {
+               font-size: 70px;
+            }
+            button {
+               width: 200px;
+               height: 50px;
+               font-size: 1.3rem;
+            }
+         }
+      }
+      @media (max-width: 360px) {
+         header {
+            li {
+               font-size: 0.8rem;
             }
          }
       }
